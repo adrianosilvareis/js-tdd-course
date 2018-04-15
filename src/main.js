3aab3f916f07036d7213c5cf2c04889f15981e2d
Adriano Reis <adriano_silvareis@hotmail.com>
2018-04-15 17:31:50 -0300

    remove função não utilizada

--- src/main.js
+++ src/main.js
@@ -3,13 +3,4 @@ const sub = (num1, num2) => num1 - num2;
 const mult = (num1, num2) => num1 * num2;
 const div = (num1, num2) => (num2 === 0 ? 'Não é possível divisão por zero!' : num1 / num2);
 
-
-function foo(){
-  return 'foo';
-};
-
-function bar(){
-  return 'bar';
-};
-
 export { sum, sub, mult, div };
